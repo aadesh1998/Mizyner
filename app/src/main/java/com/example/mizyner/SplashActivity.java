e4a82dc7a7537ac4e0a195aa0238dc82f64e37b2
aadesh1998 <aadeshmahule1998a@gmail.com>
2019-09-11 13:37:16 +0530

    splash screen

---
+++ app/src/main/java/com/example/mizyner/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.example.mizyner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 1000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeActivity=new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(homeActivity);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
